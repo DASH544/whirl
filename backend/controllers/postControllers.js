4e1f95c7d8a35fee76445fa4aefe43be6741b3cd
Dawar Shah <dawarshah544@gmail.com>
2025-02-11 23:34:52 +0530

    postControllers

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -52,12 +52,35 @@ export const getAllPosts = async (req, res) => {
     try {
   const posts = await PostModel.find({ type: "post" })
     .sort({ createdAt: -1 })
-    .populate("owner");
+    .populate("owner","-password");
     const reels = await PostModel.find({ type: "reel" })
     .sort({ createdAt: -1 })
-    .populate("owner");
+    .populate("owner","-password");
     res.status(200).json({posts,reels})
 } catch (error) {
         res.status(500).json(error.message)
 }
 };
+export const likeUnlikePost=async (req,res)=>
+  {
+    try {
+      const post=await PostModel.findById(req.params.id)
+      if(!post) return res.status(404).json({message:"No Post Found"})
+      if(post.likes.includes(req.user._id))
+        {
+          const index=post.likes.indexOf(req.user._id)
+          post.likes.splice(index,1)
+          await post.save()
+          res.status(201).json({message:"Post Unliked"})
+        }
+        else
+        {
+          post.likes.push(req.user._id)
+          await post.save()
+          res.status(201).json({message:"Post Liked"})
+        }
+
+    } catch (error) {
+      res.status(500).json(error.message)
+    }
+  }
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -3,6 +3,7 @@ import { isAuth } from "../middlewares/isAuth.js";
 import {
   deletePost,
   getAllPosts,
+  likeUnlikePost,
   newPost,
 } from "../controllers/postControllers.js";
 import uploadFile from "../middlewares/multer.js";
@@ -11,4 +12,5 @@ const router = express.Router();
 router.post("/new", isAuth, uploadFile, newPost);
 router.delete("/:id", isAuth, deletePost);
 router.get("/all", isAuth, getAllPosts);
+router.post("/like/:id",isAuth,likeUnlikePost)
 export default router;
